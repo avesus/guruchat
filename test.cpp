80f029d8562d8504ddc0b93b1843029589e82652
Ivan Borisenko <avesus8@gmail.com>
2014-10-21 16:48:51 +0400

    Test

--- test.cpp
+++ test.cpp
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+void foo()
+{
+  
+}
+
 int main(void)
 {
+  foo();
+  
   printf("This program has been created remotely from my iPhone in vim via ssh on my Mac mini!\n");
   return 0;
 } 
